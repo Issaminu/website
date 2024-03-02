208846f4e986394835b0a64fac9308d6b09e7006
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2024-03-02 10:09:31 +0100

    Add markdown support and plugins to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,55 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeSlug from "rehype-slug";
+import rehypeToc from "rehype-toc";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true
-    }
+      enabled: true,
+    },
   }),
-  integrations: [tailwind(), react(), icon()]
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeHeadingIds,
+      [rehypeAutolinkHeadings, { behavior: "wrap" }],
+      [rehypeExternalLinks, { target: "_blank" }],
+      [
+        rehypeToc,
+        {
+          headings: ["h1", "h2"],
+          customizeTOC(toc) {
+            return {
+              type: "element",
+              tagName: "details",
+              properties: {
+                class: "toc-wrapper",
+              },
+              children: [
+                {
+                  type: "element",
+                  tagName: "summary",
+                  properties: {
+                    class: "toc-summary",
+                  },
+                  children: [{ type: "text", value: "Table of Contents" }],
+                },
+                toc,
+              ],
+            };
+          },
+        },
+      ],
+    ],
+  },
+  integrations: [tailwind(), react(), icon(), mdx()],
 });
