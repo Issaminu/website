1c3a973bb4f9f37a382df678f2c3c85b8944a2e8
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2024-03-04 12:05:17 +0100

    Switch to Astro Collections for blog

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    identifier: z.string(),
+    date: z.date(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
