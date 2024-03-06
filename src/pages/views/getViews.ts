e525ed745a547abf18db99110726a82ab6a9b091
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2024-03-06 18:07:16 +0100

    Create API route for page views

---
+++ src/pages/views/getViews.ts
@@ -0,0 +1,49 @@
+import { kv } from "@vercel/kv";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+  const url = new URL(request.url);
+  const identifier = url.searchParams.get("identifier");
+
+  if (!identifier) {
+    return new Response(
+      JSON.stringify({
+        error: "No ID provided",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+  const views = await kv.get(identifier);
+  return new Response(
+    JSON.stringify({
+      views: views || 0,
+    })
+  );
+};
+
+export const PATCH: APIRoute = async ({ request }) => {
+  const body = await request.json();
+  const identifier = body.identifier;
+
+  if (!identifier) {
+    return new Response(
+      JSON.stringify({
+        error: "No ID provided",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  await kv.incr(identifier);
+  const views = await kv.get(identifier);
+
+  return new Response(
+    JSON.stringify({
+      views: views || 0,
+    })
+  );
+};
