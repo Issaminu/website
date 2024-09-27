b081b1c29558f3e673fc91f30b1321f3557f3e12
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2024-09-27 17:42:24 +0100

    Switch to attributes for ShaderGradient configuration

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -24,8 +24,36 @@ const GradientBackground = () => {
       }}
     >
       <ShaderGradient
-        control="query"
-        urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.6&cAzimuthAngle=0&cDistance=2.8&cPolarAngle=80&cameraZoom=9.1&color1=%23145580&color2=%230065ca&color3=%231e1e21&destination=onCanvas&embedMode=off&envPreset=dawn&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=0.2&positionX=0&positionY=0&positionZ=0&range=disabled&rangeEnd=40&rangeStart=0&reflection=0&rotationX=0&rotationY=0&rotationZ=0&shader=defaults&toggleAxis=false&type=waterPlane&uAmplitude=0&uDensity=1.2&uFrequency=0&uSpeed=0.1&uStrength=1&uTime=7.9&wireframe=false&zoomOut=false"
+        control="props"
+        animate="on"
+        brightness={0.6}
+        cAzimuthAngle={0}
+        cDistance={2.8}
+        cPolarAngle={80}
+        cameraZoom={9.1}
+        color1="#145580"
+        color2="#0065ca"
+        color3="#1e1e21"
+        envPreset="dawn"
+        lightType="3d"
+        grain="off"
+        positionX={0}
+        positionY={0}
+        positionZ={0}
+        reflection={0}
+        rotationX={0}
+        rotationY={0}
+        rotationZ={0}
+        type="waterPlane"
+        uAmplitude={0}
+        uDensity={1.2}
+        uFrequency={0}
+        uSpeed={0.1}
+        uStrength={1}
+        uTime={7.9}
+        wireframe={false}
+        toggleAxis={false}
+        zoomOut={false}
       />
     </ShaderGradientCanvas>
   );
